55f89033f056686499d09cbed7c725bf0a8601c9
rabidpoobear <rabidpoobear@d0423374-9d30-0410-b03d-bf706b1c5877>
2007-03-08 23:42:30 +0000

    Added the up and down arrows as movement keys in the keymap.py file.

--- Raycaster/trunk/keymap.py
+++ Raycaster/trunk/keymap.py
@@ -2,6 +2,8 @@ from pygame.locals import *
 action = {
  K_LEFT:'rotateleft',
  K_RIGHT:'rotateright',
+ K_UP:'moveforward',
+ K_DOWN:'movebackward',
  K_w:'moveforward',
  K_s:'movebackward',
  K_a:'strafeleft',
